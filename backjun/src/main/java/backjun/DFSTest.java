4f6865f84a8ab1720865f2247000e229ee833f8a
tak19 <tso5245@naver.com>
2022-12-25 22:25:14 +0900

    DFS-Test

--- backjun/src/main/java/backjun/DFSTest.java
+++ backjun/src/main/java/backjun/DFSTest.java
@@ -16,7 +16,7 @@ public class DFSTest {
 		visit = new boolean[n+1];
 		
 		//정점의 수 만큼 List 생성
-		ArrayList<Integer>[] list = new ArrayList[n];
+		ArrayList<Integer>[] list = new ArrayList[n+1];
 		for(int i = 0 ; i < list.length ; i ++) {
 			list[i] = new ArrayList<>();
 		}
@@ -25,34 +25,48 @@ public class DFSTest {
 		for(int i = 0 ; i < m ; i ++) {
 			int start = sc.nextInt();
 			int finish = sc.nextInt();
+			//양방향 연결이기 때문에 서로 넣어줌
 			list[start].add(finish);
+			list[finish].add(start);
+			
 		}
 		
+		StringBuilder sb = new StringBuilder();
+
 		
+		//list 크기 만큼 반복... 만약 리스안에 값이 없다면 다음 리스트로 돌아감..
 		for(int i = 0 ; i < list.length ; i ++) {
 			//리스트에 값이 없다면 다음 노드의 간선을 확인함
 			if(list[i].size() == 0) {
 				continue;
 			}
 			list[i].sort(null);
-			for(int j = 0 ; j < list[i].size() ; j++) {
-				//System.out.print(list[i].get(j));
-				//현재리스트와 방문불리언, 시작번호 넘김
-				dfs(list[i],visit,v);
-			}
+//			for(int j = 0 ; j < list[i].size() ; j++) {
+//				//System.out.print(list[i].get(j));
+//				//현재리스트와 방문불리언, 시작번호 넘김
+//			}
+			
 		}
-		//System.out.println();
-	}
-	
-	private static void dfs(ArrayList<Integer> list, boolean[] visit,int v) {
+		//정렬된 리스트를 가지고 방문시작
+		dfs(list,visit,v,sb);
+		
+		
+		System.out.println(sb);
+	}	
+	private static void dfs(ArrayList<Integer>[] list, boolean[] visit, int v, StringBuilder sb) {
 		//방문했다면 
 		if(visit[v]) {
 			return;
 		}else {  //방문 x 노드라면
+			//방문 표시 후 해당 노드 StringBuilder에 추가
 			visit[v] = true;
-//			while( list[v] ) {
-//				
-//			}
+			sb.append(v + " ");
+			
+			//해당 노드의 크기만큼 반복함
+			for(int i = 0 ; i < list[v].size(); i ++) {
+				dfs(list,visit,list[v].get(i),sb);
+			}
+
 		}
 		
 
