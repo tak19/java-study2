6f4e4afd29ddf2c5192f26f4fecec04ee90a6c78
tak19 <tso5245@naver.com>
2022-12-26 00:13:02 +0900

    BFS-Test

---
+++ backjun/src/main/java/backjun/BFSTest.java
@@ -0,0 +1,117 @@
+package backjun;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BFSTest {
+	static boolean[] visit;
+	static Queue<Integer> queue = new LinkedList<Integer>();
+	public static void main(String[] args) {
+		//list에 값이 없으면 null 주는것 같음
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); //정점의 수
+		int m = sc.nextInt(); //간선의 수
+		int v = sc.nextInt(); //시작 번호
+		queue.add(v);  //시작 노드 큐에 넣음
+		
+		//방문한 노드 확인할 불리언값
+		visit = new boolean[n+1];
+
+		//정점의 수 만큼 List 생성
+		ArrayList<Integer>[] list = new ArrayList[n+1];
+		for(int i = 0 ; i < list.length ; i ++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		
+		for(int i = 0 ; i < m ; i ++) {
+			int start = sc.nextInt();
+			int finish = sc.nextInt();
+			//양방향 연결이기 때문에 서로 넣어줌
+			list[start].add(finish);
+			list[finish].add(start);
+			
+		}
+		
+		StringBuilder sb = new StringBuilder();
+
+		
+		//list 크기 만큼 반복... 만약 리스안에 값이 없다면 다음 리스트로 돌아감..
+		for(int i = 0 ; i < list.length ; i ++) {
+			//리스트에 값이 없다면 다음 노드의 간선을 확인함
+			if(list[i].size() == 0) {
+				continue;
+			}
+			list[i].sort(null);
+//			for(int j = 0 ; j < list[i].size() ; j++) {
+//				//System.out.print(list[i].get(j));
+//				//현재리스트와 방문불리언, 시작번호 넘김
+//			}
+			
+		}
+		//정렬된 리스트를 가지고 방문시작
+		bfs(list,visit,v,sb,queue);
+		
+		
+		System.out.println(sb);
+	}	
+	private static void bfs(ArrayList<Integer>[] list, boolean[] visit, int v, StringBuilder sb, Queue<Integer> queue) {
+		//방문했다면 
+		if(visit[v]) {
+			//방문한 경우 해당 노드를 보내고 다시 실행 -- 큐에 공간 있는지 확인
+			if(queue.size() > 0 ) {
+				bfs(list,visit,queue.poll(),sb,queue);
+			}
+			
+		}else {  //방문 x 노드라면
+			//방문 표시 후 해당 노드 StringBuilder에 추가
+			visit[v] = true;
+			sb.append(v + " ");
+			
+			//해당 노드와 연결된 노드를 큐에 저장
+			for(int i = 0 ; i < list[v].size(); i ++) {
+				int qadd = list[v].get(i);
+				//System.out.println(qadd);
+				queue.add(qadd);
+			}
+			
+			//헤드노드는 방문처리 후 poll -- size 확인
+			if(queue.size() > 0) {
+				//System.out.println(queue.poll());
+				queue.poll();
+				bfs(list,visit,queue.peek(),sb,queue);
+			}else {
+				return;
+			}
+			
+
+		}
+
+	}
+
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
