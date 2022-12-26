4d5d2d8a17d23bcfc59b0d16252ad04f567b2833
tak19 <tso5245@naver.com>
2022-12-26 09:54:40 +0900

    Generic Method + 가변

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,21 +2,65 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//		
+//	}
+//	public void println(boolean b) {
+//		System.out.println(b);
+//		
+//	}
+//	public void println(double d) {
+//		System.out.println(d);
+//		
+//	}
+//	public void println(String s) {
+//		System.out.println(s);
+//		
+//	}
+	
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println( t);
+		}
+		
 		
 	}
-	public void println(boolean b) {
-		System.out.println(b);
+	
+	//재너릭의 사용 제한을 위해 상속,슈퍼를 통해 기능 제한 가능
+//	public <T extends Number> void println(T t) {
+//		System.out.println( t);
+//		
+//	}
+	
+	//가변 파라매터 표시 ... 
+	public int sum(Integer... nums) {
+		
+		int s = 0;
+		for(Integer n :nums) {
+			s += n;
+		}
+		return s;
 		
 	}
-	public void println(double d) {
-		System.out.println(d);
-		
-	}
-	public void println(String s) {
-		System.out.println(s);
-		
-	}
+	
+	
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -9,5 +9,15 @@ public class PrinterTest {
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+		
+		printer.println( "홍길동",1 ,2 ,3 );
+		
+		//가변 파라매타의 경우 오버로딩이 어렵다
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1,2));
+		System.out.println(printer.sum(1,2,3,4,5));
+		System.out.println(printer.sum(1,2,3,4,5,6,7,8,9,10));
+		
+		
 	}
 }
