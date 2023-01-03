9ebd32563e8fd9c86f9999c4d8a8f611a995db6b
tak19 <tso5245@naver.com>
2023-01-04 03:14:48 +0900

    bfs

--- backjun/src/main/java/bfs/BFSTest.java
+++ backjun/src/main/java/bfs/BFSTest.java
@@ -1,4 +1,4 @@
-package backjun;
+package bfs;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
--- backjun/src/main/java/dfs/DFSTest.java
+++ backjun/src/main/java/dfs/DFSTest.java
@@ -1,4 +1,4 @@
-package backjun;
+package dfs;
 
 import java.util.ArrayList;
 import java.util.Scanner;
