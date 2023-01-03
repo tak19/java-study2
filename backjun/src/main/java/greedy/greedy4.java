8051f3b47b794772308dec37f38dd91cd14e2a7b
tak19 <tso5245@naver.com>
2023-01-04 03:48:18 +0900

    greedy-신입사원(1946)

--- backjun/src/main/java/greedy/greedy4.java
+++ backjun/src/main/java/greedy/greedy4.java
@@ -1,60 +1,52 @@
 package greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class greedy4 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(bf.readLine());
 		
+		StringBuffer sb = new StringBuffer();
 		//TeseCase 실행
 		for(int i = 0 ; i < t ; i ++) {
-			System.out.println(i+"번재 테스트 시작");
+			//System.out.println(i+"번재 테스트 시작");
 			
-			int n = sc.nextInt(); // 인원 수
+			int n = Integer.parseInt(bf.readLine()); // 인원 수
 			int[][] arr = new int [n][2];
 			
 			//사원의 성적 입력받음
 			for(int j = 0 ; j < n ; j ++) {
-				arr[j][0] = sc.nextInt();
-				arr[j][1] = sc.nextInt();
+				String[] s = bf.readLine().split(" ");
+				arr[j][0] = Integer.parseInt(s[0]);
+				arr[j][1] = Integer.parseInt(s[1]);
 			}
 			
 			Arrays.sort(arr,(o1, o2) -> {
 			    return o1[0]-o2[0]; // 첫번째 숫자 기준 오름차순
 			});
 			
-			int total = 0 ;
-			for(int j = 0 ; j < n ; j ++) {
+			int total = 1 ;
+			//첫번째 사람의 두번째 시험점수
+			int min = arr[0][1];
+			
+			for(int j = 1 ; j < n ; j ++) {
 				//각 사람별 기준을 잡고 반복 실행
-				int second = arr[j][1];
-				int cnt = 1;
-				//j인덱스 보다 작은 경우 = 2번째 시험 점수가 나보다 높아야함
-				for(int z = 0 ; z < j ; z ++) {
-					if(second < arr[z][1]) {
-						//System.out.println("2번째 시험점수가 나보다 높음");
-						cnt++;
-					}
+				if( min > arr[j][1]) {
+					total++;
+					min = arr[j][1];;
 				}
-				//j인덱스 보다 큰 경우 = 2번째 시험 점수가 나보다 낮아야함
-				for (int z = j; z < n; z++) {
-					if(second > arr[z][1]) {
-						//System.out.println("2번째 시험점수가 나보다 낮음");
-						cnt++;
-					}
-				}
-				total = Math.max(total, cnt);
+				
+				
 			}
-			System.out.println(total);
-			
-			
-			
-			
+			sb.append(total + "\n");
 			
 		}
-		
+		System.out.println(sb);
 	}
 
 }
