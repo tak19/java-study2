10f865525e70868268faa84f5b1fc21c09546112
tak19 <tso5245@naver.com>
2023-01-04 23:50:43 +0900

    Input,Output - 빠른 A+B(15552)

---
+++ backjun/src/main/java/backjun/fast.java
@@ -0,0 +1,21 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class fast {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuffer sb = new StringBuffer();
+		for(int i=0 ; i <t ; i++) {
+			String[] s = br.readLine().split(" ");
+			sb.append( Integer.parseInt(s[0]) + Integer.parseInt(s[1]) + "\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
