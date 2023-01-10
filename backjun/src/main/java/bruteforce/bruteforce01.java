8e890902e8c8fb8378af3c0257550d5c701ff4bc
tak19 <tso5245@naver.com>
2023-01-11 00:07:31 +0900

    bruteforce - 한수(1065)

---
+++ backjun/src/main/java/bruteforce/bruteforce01.java
@@ -0,0 +1,39 @@
+package bruteforce;
+
+import java.util.Scanner;
+
+public class bruteforce01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+
+		int cnt = 0;
+		int ck = 0;
+		if (n >= 100) {
+			cnt = 99;
+			for (int i = 100; i <= n; i++) {
+				ck = 0;
+				char[] arr = String.valueOf(i).toCharArray();
+				 int mul = arr[0] - arr[1];
+				for (int j = 1; j < arr.length - 1; j++) {
+					if (mul != arr[j] - arr[j + 1]) { // 차가 같지 않은 경우임
+						ck = 1;
+						break;
+					}
+
+				}
+				if (ck != 1) {
+					cnt++;
+				}
+
+			}
+		} else {
+			cnt = n;
+		}
+
+		System.out.println(cnt);
+	}
+
+}
