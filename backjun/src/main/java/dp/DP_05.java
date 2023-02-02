8fe914810918309bdae08082db23a00da0e3dba6
tak19 <tso5245@naver.com>
2023-02-02 19:23:15 +0900

    DP - 동물원(1309)

--- backjun/src/main/java/dp/DP_05.java
+++ backjun/src/main/java/dp/DP_05.java
@@ -3,7 +3,7 @@ package dp;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class DP05 {
+public class DP_05 {
 	static int[] lion;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
