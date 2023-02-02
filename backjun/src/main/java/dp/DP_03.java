ad2df16797f69a7541c8f0a2fc07370007bd291b
tak19 <tso5245@naver.com>
2023-02-02 19:21:27 +0900

    DP - 강의실 배정(11000)

--- backjun/src/main/java/dp/DP_03.java
+++ backjun/src/main/java/dp/DP_03.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class DP03 {
+public class DP_03 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
