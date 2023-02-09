35c03025282c12a11af2e59ed1f62801401947b3
tak19 <tso5245@naver.com>
2023-02-09 22:24:53 +0900

    prime - 신기한 소수(2023)

--- backjun/src/main/java/etc/etc_20.java
+++ backjun/src/main/java/etc/etc_20.java
@@ -6,63 +6,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class etc_20 {
-
-	static boolean[] prime;
-	static List<Integer> plist = new ArrayList<Integer>();
+	static StringBuilder sb = new StringBuilder();
+	static int n;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 
-		int max = (int) Math.pow(10, n);
-		prime = new boolean[max];
-		primeCk(max);
-		prime[1] = prime[0] = true;
-		StringBuilder sb = new StringBuilder();
-		
-		//신기한지 판단 -> 안에 자리 수가 소수인지는 boolean으로 보면될듯
-		for(int i = 0 ; i < plist.size() ; i++) {
-			
-			boolean ck = false;
-			int a = plist.get(i);
-			//나눠가며 판단
-			for(int j = 1 ; j <= n-1 ; j++) {
-				int sub = (int) Math.pow(10, j);
-				if( prime[  a/sub   ] ) { //소스가 아니라면
-					ck = true;
-					break;
-				}
-			}
-			if( !ck ) {
-				sb.append(a).append("\n");
-			}
-			
-			
+		//n 자리 소수는 -> 2, 3, 5, 7로 시작해야함 
+		int[] start = {2, 3, 5, 7};
+		for(int i = 0 ; i < 4 ; i++) {
+			plusCnt( start[i] , 1);
 		}
-		
 		System.out.println(sb);
 		
 	}
 
-	//소수 판별하기 --> 아라토무시기 체
-	private static void primeCk(int max) {
-		
-		for(int i = 2 ; i * i <= max ; i++) {
-			if( !prime[i] ) {
-				for(int j = i*i ; j < max ; j += i) {
-					//System.out.println("j :" + j);
-					prime[j] = true;
-				}
-			}
-			
+	//자릿 수 늘려 주면서 소수 체크
+	private static void plusCnt(int num, int cnt) {
+		if( cnt == n ) {
+			sb.append(num).append("\n");
+			return;
 		}
-		//n 자릿수의 소수가 다 넣어줌
-		for (int i = max/10; i < max; i++) {
-			if (!prime[i])
-				plist.add(i);
+		num *= 10;
+		for(int i = 1 ; i < 10; i++) {
+			if( primeCk(num+i) ) {
+				plusCnt( num+i , cnt + 1);
+			}
 		}
 		
 	}
+
+	private static boolean primeCk(int prime) {
+		//나누어 떨어지는 수 있으면 false 반환
+		for(int i = 2; i <= Math.sqrt(prime) ; i++) {
+			if( prime % i == 0 ) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+
 	
 }
 
