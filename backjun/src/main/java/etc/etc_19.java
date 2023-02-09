4f91e1e73a6dd7613f9b816be363ccd878bcc119
seungok <tso5245@naver.com>
2023-02-09 15:05:47 +0900

    Queue - 카드2(2164)

--- backjun/src/main/java/etc/etc_19.java
+++ backjun/src/main/java/etc/etc_19.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class ect_11 {
+public class etc_19 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
