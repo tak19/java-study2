d027e8de08ee54d6c908e1e09742a2c91095e256
seungok <tso5245@naver.com>
2023-02-09 15:04:31 +0900

    Queue - 카드2(2164)

--- backjun/src/main/java/etc/etc_18.java
+++ backjun/src/main/java/etc/etc_18.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class ect_18 {
+public class etc_18 {
 	private static StringBuilder sb = new StringBuilder();
 	static int n, result;
 	static int[] dan,sin;
