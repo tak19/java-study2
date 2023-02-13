507a0284e197db372fc8cbaaa903d727ff655746
tak19 <tso5245@naver.com>
2023-02-13 21:57:31 +0900

    Basic - 암호문1(1228)

--- swea/src/main/java/basic/basic_01.java
+++ swea/src/main/java/basic/basic_01.java
@@ -1,11 +1,11 @@
-package needClean;
+package basic;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class etc_01 {
+public class basic_01 {
 	private static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
