516f4d6c57d133d30f2bc96f0e1f00ad61a1f1e5
tak19 <tso5245@naver.com>
2023-02-18 17:32:55 +0900

    Combination - 순열의 순서(1722)

--- backjun/src/main/java/combination/combination_04.java
+++ backjun/src/main/java/combination/combination_04.java
@@ -15,85 +15,67 @@ public class combination_04 {
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		arr = new int[n];
-		//순열 저장함
-		for(int i = 0 ; i < n ; i++) {
-			arr[i] = i+1;
+
+		//순열의 경우의 수 계산 - 총 20 까지 있음 - 받은 n만큼만 계산
+		long[] pec = new long[n+1];
+		pec[0] = 1;
+		for( int i = 1; i <= n ; i++ ) {
+			pec[i] = i * pec[i-1];
 		}
+		boolean[] isSelect = new boolean[n+1]; 
+		arr = new int[n];
+
 		//문제 입력 받아욧
 		st = new StringTokenizer(br.readLine());
-		int what = Integer.parseInt(st.nextToken());
+		int what = Integer.parseInt(st.nextToken()); //원하는 유형의 문제
+		long cnt = 0; //20! 이기때문에 long으로 값 받음
 
-		int cnt = 0;
-		int[] t = new int[n];
 
 		if( what == 1) { //순서 물어보나?
-			cnt = Integer.parseInt(st.nextToken());
+			cnt = Long.parseLong(st.nextToken()); //물어보는 순열의 순서
+
+			for(int i = 0 ; i < n ; i++) {
+				for(int j = 1 ; j <= n ; j++) {
+					if(isSelect[j]) {
+						continue;
+					}
+					//자릿수 계산함, 팩토리얼 계산값보다 크면 해당 자리수는 아니기때문에 넘어감
+					if( cnt > pec[n-i-1] ) {
+						cnt -= pec[n-i-1];
+					}else {//팩토리얼 계산보다 작다면 해당 자리수에 도착한것임
+						arr[i] = j;
+						isSelect[j] = true;
+						break;
+					}
+
+				}
+			}
+			for(int i = 0 ; i < n ; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+
+
 		}else {// 순열 차례 물어보나?
 			for(int i = 0 ; i < n ; i++) {
-				t[i] = Integer.parseInt(st.nextToken());
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-		}
-
-		Arrays.sort(arr);
-		int stop = 1;
-		if(what == 1) {
-			do {
-				if( stop == cnt ) {//원하는 순서에 도착하믄
-					for(int i = 0 ; i < n ; i++) {
-						sb.append(arr[i] + " ");
+			//차례를 계산한다
+			for(int i = 0 ; i < n ; i++) {
+				for(int j = 1 ; j < arr[i] ; j++) { //해당 자리수만큼 순열 수를 계산한다.
+					if(isSelect[j]) {
+						continue;
 					}
-					break;
+					cnt += pec[n-1-i];
 				}
+				isSelect[arr[i]] = true;
+			}
+			sb.append(cnt + 1);
 
-				stop++;
-			} while (npcom(arr));
-		}else {
-			
-			do {
-				if(Arrays.equals(arr, t)) {
-					sb.append(stop);
-					break;
-				}
-				stop++;
-			} while (npcom(arr));
 		}
+
+
 
 		System.out.println(sb);
 
 	}
-
-
-
-	private static boolean npcom(int[] input) {
-		int n = input.length;
-
-		int i = n -1;
-		while(i > 0 && input[i-1] >= input[i]) {
-			i--;
-		}
-		if( i == 0 ) {
-			return false;
-		}
-
-		int j = n -1;
-		while( input[i-1] >= input[j] ) {
-			j--;
-		}
-		swap(input,i-1,j);
-
-		int k = n -1;
-		while( i <= k ) {
-			swap(input, i++, k--);
-		}
-
-		return true;
-	}
-
-	private static void swap(int[] input, int i, int j) {
-		int tem = input[i];
-		input[i] = input[j];
-		input[j] = tem;
-	}
-
 }
