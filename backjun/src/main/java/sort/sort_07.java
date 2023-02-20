7a9636d09892b64712b0497997c546408f2505b3
tak19 <tso5245@naver.com>
2023-02-20 21:59:28 +0900

    Sort - N번째 큰 수(2075)

---
+++ backjun/src/main/java/sort/sort_07.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class sort_07 {
+	public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n*n];
+        StringTokenizer st;
+        int cnt= 0;
+        for(int i = 0; i < n ; i ++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n ; j++){
+                arr[cnt++] = Integer.parseInt(st.nextToken()); 
+            }
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length - n]);
+
+
+    }
+}
