ed72b7d31580dfe1269b434d518ef64f56317ab8
tak19 <tso5245@naver.com>
2023-03-02 01:28:28 +0900

    홈 방범 푸는중..

---
+++ swea/src/main/java/etc/etc_12.java
@@ -0,0 +1,44 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class etc_12 {
+	static int N,M,allCash;
+	static boolean[][] map;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for(int test = 1 ; test <= T ; test++) {
+			sb.append("#").append(test).append(" ");
+			
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 지도 크기
+			M = Integer.parseInt(st.nextToken()); // 비용
+			
+			int homeCnt = 0;
+			//지도 입력 받기
+			map = new boolean[N][N];
+			for(int i = 0 ; i < N ; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0 ; j < N ; j++) {
+					//집이 있으면 true로 처리함
+					if( Integer.parseInt(st.nextToken()) == 1 ) {
+						homeCnt++;
+						map[i][j] = true;
+					}
+				}
+			}
+			allCash = homeCnt * M; //전체 집개수 * 비용 => 전체 비용을 계산함!!
+			
+			//전체 비용보다 운용 비용이 커지면 탐색 x
+			//마름모: (n*2)-1 만큼 반복되고, 중앙 n 에서 (n*2)-1 만큼 색칠됨.. 2개씩 늘어나다가 2개씩 감소하는 구조 
+
+		}
+		System.out.println(sb);
+
+	}
+}
