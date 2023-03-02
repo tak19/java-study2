17a1a29b2272843d6cca822d2b2adf682b5c583f
seungok <tso5245@naver.com>
2023-03-02 17:54:38 +0900

    홈 서비스 수정 필요

--- swea/src/main/java/etc/etc_12.java
+++ swea/src/main/java/etc/etc_12.java
@@ -14,11 +14,11 @@ public class etc_12 {
 		StringTokenizer st;
 		for(int test = 1 ; test <= T ; test++) {
 			sb.append("#").append(test).append(" ");
-			
+
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken()); // 지도 크기
 			M = Integer.parseInt(st.nextToken()); // 비용
-			
+
 			int homeCnt = 0;
 			//지도 입력 받기
 			map = new boolean[N][N];
@@ -33,12 +33,33 @@ public class etc_12 {
 				}
 			}
 			allCash = homeCnt * M; //전체 집개수 * 비용 => 전체 비용을 계산함!!
-			
+
 			//전체 비용보다 운용 비용이 커지면 탐색 x
-			//마름모: (n*2)-1 만큼 반복되고, 중앙 n 에서 (n*2)-1 만큼 색칠됨.. 2개씩 늘어나다가 2개씩 감소하는 구조 
+			//마름모: (n*2)-1 만큼 반복되고, 중앙 n 에서 (n*2)-1 만큼 색칠됨.. 2개씩 늘어나다가 2개씩 감소하는 구조
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j < N ; j++) {
+					homeService(i,j);
+				}
+			}
 
 		}
 		System.out.println(sb);
 
 	}
+	//홈 서비스 갑니다~!  
+	private static void homeService(int x, int y) {
+		int K = 1;
+		while( K * K + (K - 1) * (K - 1) > allCash ) {
+			
+			for(int i = 0 ; i < (K * 2) -1 ; i++ ) {
+				
+			}
+			
+			K++; // 홈 서비스 증가
+		}
+	}
 }
+
+
+
+
