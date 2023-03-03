f8cc767b31e1575ba66141c78250a2a33857a752
seungok <tso5245@naver.com>
2023-03-03 09:42:02 +0900

    모의 SW 역량테스트 - 핀볼게임(5650)

--- swea/src/main/java/etc/etc_13.java
+++ swea/src/main/java/etc/etc_13.java
@@ -3,7 +3,7 @@ package etc;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//5650 핀볼게임
 public class etc_13 {
 	static int N,startX,startY,result;
 	//상하좌우 순
@@ -13,19 +13,19 @@ public class etc_13 {
 	static Pos[] posList;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine().trim());
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		for(int test = 1 ; test <= T ; test++) {
 			sb.append("#").append(test).append(" ");
 
-			N = Integer.parseInt(br.readLine()); // 게임장 크기
+			N = Integer.parseInt(br.readLine().trim()); // 게임장 크기
 			map = new int[N][N]; //게임장 생성
 			posList = new Pos[10];
 			int wormCnt = 0;
 			//맵 입력
 			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine().trim());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 					//Worm Hole
@@ -36,15 +36,18 @@ public class etc_13 {
 				}
 			}
 			result = 0;
+
 			//게임 시작
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 					//빈 공간이라면 시작점을 저장해 두고 탐색 시작한다.
 					if(map[i][j] == 0) {
+						//System.out.println(" 시작  X: " +i + " 시작  Y: " + j);
 						startX = i;
 						startY = j;
 						for(int dir = 0 ; dir < 4 ; dir++) {
 							dfs(i,j,0,dir);
+							//System.out.println(dir + " 방향 종료");
 						}
 					}
 				}
@@ -62,88 +65,92 @@ public class etc_13 {
 		//이동 좌표
 		int temX = i + dx[dir];
 		int temY = j + dy[dir];
-
-		//범위 벗어나거나 5번 블록 만나면 반대로 똑같은 거리를 감 따라서 (거리 *2) 하고 종료
-		if( !canGo(temX,temY) || map[temX][temY] == 5 ) {
-			getPoint(point);
-			return;
-		}
-
-		//블랙홀 이라면 종료
-		if( map[temX][temY] == -1 ) {
+		//시작점으로 돌아온다면!!
+		if( temX == startX && temY == startY ) {
 			result = Math.max(result, point);
 			return;
-		}
+		}else {
+			//범위 벗어나거나 5번 블록 만나면 반대로 똑같은 거리를 감 따라서 (거리 *2) 하고 종료
+			if( !canGo(temX,temY) || map[temX][temY] == 5 ) {
+				getPoint(point);
+				return;
+			}
 
-		//이동 공간이 빈공간이 아니라면.. 후보: 블록(5번 블록 제외), 웜홀  (나는 	상하좌우 )
-		/*
-		 * {-1,1,0,0};
-		 * {0,0,-1,1};
-		 */
-		if( 0 < map[temX][temY]  && map[temX][temY] <= 4) {
-			switch (map[temX][temY]) {
-			case 1: {
-				//우 + 상 이랑 만나면 반대로 튕김
-				if( dir == 0 || dir == 3 ) {
-					getPoint(point);
-					return;
-				}else {
-					point++;
-					dir = chageDir(dir,1);
-				}
-				break;
+			//블랙홀 이라면 종료
+			if( map[temX][temY] == -1 ) {
+				result = Math.max(result, point);
+				return;
 			}
-			case 2: {
-				//우+하 랑 만나면 반대로 튕김
-				if( dir == 1 || dir == 3 ) {
-					getPoint(point);
-					return;
-				}else {
-					point++;
-					dir = chageDir(dir,2);
-				}
-				break;
-			}
-			case 3: {
-				//하+좌 만나면 반대로 튕김
-				if( dir == 1 || dir == 2 ) {
-					getPoint(point);
-					return;
-				}else {
-					point++;
-					dir = chageDir(dir,3);
-				}
-				break;
-			}
-			case 4: {
-				//상+좌 만나면 반대로 튕김
-				if( dir == 0 || dir == 2 ) {
-					getPoint(point);
-					return;
-				}else {
-					point++;
-					dir = chageDir(dir,4);
-				}
-				break;
-			}
-			}
-		}
-		//웜홀 이라면
-		if( 5 < map[temX][temY] ) {
-			for(int k = 0 ; k < 10 ; k++) {
-				Pos p = posList[k];
-				//같은 번호의 웜홀이면서 다른 위치에 있는 웜홀 이라면
-				if( map[temX][temY] == p.wormNum && temX != p.x && temY != p.y ) {
-					temX = p.x;
-					temY = p.y;
+
+			//이동 공간이 빈공간이 아니라면.. 후보: 블록(5번 블록 제외), 웜홀  (나는 	상하좌우 )
+			/*
+			 * {-1,1,0,0};
+			 * {0,0,-1,1};
+			 */
+			if( 0 < map[temX][temY]  && map[temX][temY] <= 4) {
+				switch (map[temX][temY]) {
+				case 1: {
+					//우 + 상 이랑 만나면 반대로 튕김
+					if( dir == 0 || dir == 3 ) {
+						getPoint(point);
+						return;
+					}else {
+						point++;
+						dir = chageDir(dir,1);
+					}
 					break;
 				}
+				case 2: {
+					//우+하 랑 만나면 반대로 튕김
+					if( dir == 1 || dir == 3 ) {
+						getPoint(point);
+						return;
+					}else {
+						point++;
+						dir = chageDir(dir,2);
+					}
+					break;
+				}
+				case 3: {
+					//하+좌 만나면 반대로 튕김
+					if( dir == 1 || dir == 2 ) {
+						getPoint(point);
+						return;
+					}else {
+						point++;
+						dir = chageDir(dir,3);
+					}
+					break;
+				}
+				case 4: {
+					//상+좌 만나면 반대로 튕김
+					if( dir == 0 || dir == 2 ) {
+						getPoint(point);
+						return;
+					}else {
+						point++;
+						dir = chageDir(dir,4);
+					}
+					break;
+				}
+				}
+			}else if( 5 < map[temX][temY] ) { //웜홀 이라면
 
+				for(int k = 0 ; k < 10 ; k++) {
+					if( posList[k] != null ) {
+						Pos p = posList[k];
+						//같은 번호의 웜홀이면서 다른 위치에 있는 웜홀 이라면
+						if( map[temX][temY] == p.wormNum && ( temX != p.x || temY != p.y ) ) {
+							temX = p.x;
+							temY = p.y;
+							break;
+						}
+					}
+				}
 			}
 		}
+
 		dfs(temX, temY, point, dir);
-
-
 	}
 	//반대벽 방향 갈때 포인트 값 갱신 
 	private static void getPoint(int point) {
