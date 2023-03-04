49c06da82feb041f27e28f3f8dd49d18891f95af
tak19 <tso5245@naver.com>
2023-03-04 17:03:07 +0900

    bruteforce - 색종이 붙이기(17136)

--- backjun/src/main/java/bruteforce/bruteforce_07.java
+++ backjun/src/main/java/bruteforce/bruteforce_07.java
@@ -7,10 +7,11 @@ import java.util.StringTokenizer;
 public class bruteforce_07 {
 	private static StringBuilder sb = new StringBuilder();
 	static int[][] map;
+	static int[] paper;
+	static boolean[][] visit;
 	static int result;
 
 	public static void main(String[] args) throws Exception{
-		//System.setIn(new FileInputStream("res/17478_input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		map = new int[10][10];
 
@@ -22,72 +23,89 @@ public class bruteforce_07 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		result = 26; //25장 어차피 안넘음
+		paper = new int[] {0,0,0,0,0,0};
+		visit = new boolean[10][10];
 		//붙이기 시작
-		dfs(0,0,new int[6],new boolean[10][10]);
+		dfs(0,0,paper,0);
 
-
-		System.out.println(sb);
-
+		System.out.println(result == 26 ? -1 : result);
 
 	}
 	//종이 붙이기
-	private static void dfs(int row, int col, int[] paper, boolean[][] visit) {
-		//열 끝 번이라면
-		if( col == 9 ) {
-			dfs(row+1,0,paper,visit);
-			return;
-		}
-		//다 붙였다면..
-		if( row == 10) {
+	private static void dfs(int row, int col, int[] paper, int cnt) {
 
+		//다 붙였다면..
+		if( row == 9 && col == 10) {
+			result = Math.min(result, cnt);
 			return;
 		}
+
+		//열 끝 번이라면
+		if( col == 10 ) {
+			dfs(row+1,0,paper,cnt);
+			return;
+		}
+		//현재 cnt(사용종이)가 최소값보다 크다면 더 이상 계산할 필요가 없다.
+		if(cnt >= result) return; 
+
 
 		//색종이를 붙여야한다면 -> 각종 색종이 붙인다!
 		if( map[row][col] == 1 && !visit[row][col] ) {
-			for(int i = 1 ; i <= 5 ; i++) {
-				
+			for(int i = 5 ; i > 0 ; i--) {
 				//true면 진행 flase 나오면 더이상 할 필요도 읎다
-				if( ck(row,col,i) ) {
+				if( ck(row,col,i) && paper[i] < 5 ) {
+					
+					//5장 다 안썼다면
+					paper[i]++; //1장 사용
 					//붙으면 방문처리해준다요
-					for(int r = row ; r < row + i ; row++) {
-						for(int c = col ; c < col + i ; col++) {
-							visit[r][c] = true;	
-						}
-					}
-					
-					
-					
+					inpaper(row,col,i);
+
+					//다음 열 탐색
+					dfs(row,col+1,paper,cnt+1);
+					//방문 후 종이 개수 원상태로 바꾸고 뗌
+					paper[i]--;
+					outpaper(row,col,i);
+
 				}
+
 			}
 		}
-		//다음 열 탐색
-		dfs(row,col+1,paper,visit);
+		else {
+			dfs(row,col+1,paper,cnt);
 
-
-
-
+		}
 	}
-	//1이 연속되는지 확인함
+	// 색종이 제거
+	private static void outpaper(int row, int col, int size) {
+		for(int r = row ; r < row + size ; r++) {
+			for(int c = col ; c < col + size ; c++) {
+				visit[r][c] = false;
+			}
+		}
+	}
+	//색종이 붙이기
+	private static void inpaper(int row, int col, int size) {
+		for(int r = row ; r < row + size ; r++) {
+			for(int c = col ; c < col + size ; c++) {
+				visit[r][c] = true;
+			}
+		}
+	}
+	//1이 연속되는지 확인함 -> size 만큼
 	private static boolean ck(int row, int col, int size) {
 		//해당 사이즈 만큼의 종이가 들어가는가?
-		for(int r = row ; r < row + size ; row++) {
-			for(int c = col ; c < col + size ; col++) {
-				//0이 나온다면 조건 불충분함
-				if( map[r][c] == 0 ) {
+		for(int r = row ; r < row + size ; r++) {
+			for(int c = col ; c < col + size ; c++) {
+				if( r >= 10 || c >= 10 ) {
+					return false;
+				}
+				//0이 나오거나 붙이는 종이가 겹친다면 false;
+				if( map[r][c] == 0 || visit[r][c] ) {
 					return false;
 				}
 			}
 		}
-
 		return true;
 	}
-
 }
-
-
-
-
-
-
-
