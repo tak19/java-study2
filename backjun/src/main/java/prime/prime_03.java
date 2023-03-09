d7d9edf1bad17d2afdcfa9ef83b01a2ac16a37c0
tak19 <tso5245@naver.com>
2023-03-09 23:34:00 +0900

    정수론 - 소수&팰린드롬(1747)

---
+++ backjun/src/main/java/prime/prime_03.java
@@ -0,0 +1,61 @@
+package prime;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class prime_03 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		// 계산해보니 1000000 이상인 최소 팰린드롬 수는 1003001 이다
+		boolean[] isSelect = new boolean[1003002];
+		for(int i = 2 ; i < 1003002; i++ ) {
+			if( !isSelect[i] ) {
+				//소수인데 n보다 크거나 같은지
+				if( n <= i ) {
+					//그렇다면 펠린드롬 검사함
+					if( cal(i) ) {
+						System.out.println(i);
+						break;
+					}
+				}
+				//System.out.print(i + " ");
+				for(int j = i+i ; j < 1003002; j += i) {
+					isSelect[j] = true;
+				}
+			}
+		}
+		
+		
+		
+	}
+	//팰린드롬인지 확인
+	private static boolean cal(int i) {
+		Deque<Integer> dq = new ArrayDeque<>();
+		while( i != 0 ) {
+			//10으로 나눈 나머지로 자리수를 저장함
+			int a = i % 10;
+			dq.offer(a);
+			i /= 10;
+		}
+		//자리수 저장함
+		int size = dq.size();
+		//사이즈의 반만큼 실행 -> 짝수면 다 실행 홀수도 상관없음 중앙값으 대칭이기때문
+		for(int k = 0 ; k < size / 2 ; k++) {
+			int first = dq.pollFirst();
+			int last = dq.pollLast();
+			//두 원소가 같지 않다면 false 반환
+			if( first != last) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
+
+
+
