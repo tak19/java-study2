bed269d96228b6214f92a8635f8d446c80f1d498
tak19 <tso5245@naver.com>
2023-03-15 00:15:04 +0900

    정수론 - 소수&팰린드롬(1747)

--- backjun/src/main/java/prime/prime_03.java
+++ backjun/src/main/java/prime/prime_03.java
@@ -11,46 +11,30 @@ import java.util.TreeSet;
 
 public class prime_03 {
 	public static void main(String[] args) throws Exception{
-		
-//		HashMap<String, String> map = new HashMap<String, String>();
-//		map.put(1, "사람");
-//		map.put('2', "야구");
-		
-		TreeSet<Integer> s = new TreeSet<>();
-		
-		s.add(1);
-		s.add(4);
-		s.add(2);
-		s.add(1);
-		
-		for (Iterator iterator = s.iterator(); iterator.hasNext();) {
-			System.out.println(iterator.next());
-			
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		// 계산해보니 1000000 이상인 최소 팰린드롬 수는 1003001 이다
+		boolean[] isSelect = new boolean[1003002];
+		for(int i = 2 ; i < 1003002; i++ ) {
+			if( !isSelect[i] ) {
+				//소수인데 n보다 크거나 같은지
+				if( n <= i ) {
+					//그렇다면 펠린드롬 검사함
+					if( cal(i) ) {
+						System.out.println(i);
+						break;
+					}
+				}
+				//System.out.print(i + " ");
+				for(int j = i+i ; j < 1003002; j += i) {
+					isSelect[j] = true;
+				}
+			}
 		}
-		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		int n = Integer.parseInt(br.readLine());
-//		// 계산해보니 1000000 이상인 최소 팰린드롬 수는 1003001 이다
-//		boolean[] isSelect = new boolean[1003002];
-//		for(int i = 2 ; i < 1003002; i++ ) {
-//			if( !isSelect[i] ) {
-//				//소수인데 n보다 크거나 같은지
-//				if( n <= i ) {
-//					//그렇다면 펠린드롬 검사함
-//					if( cal(i) ) {
-//						System.out.println(i);
-//						break;
-//					}
-//				}
-//				//System.out.print(i + " ");
-//				for(int j = i+i ; j < 1003002; j += i) {
-//					isSelect[j] = true;
-//				}
-//			}
-//		}
-		
-		
-		
+
+
+
 	}
 	//팰린드롬인지 확인
 	private static boolean cal(int i) {
@@ -75,8 +59,3 @@ public class prime_03 {
 		return true;
 	}
 }
-
-
-
-
-
