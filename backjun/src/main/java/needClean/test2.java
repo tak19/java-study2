a52e07b149e5a9720bc2f6889c25b5c55bfc8757
tso5245 <tso5245@naver.com>
2023-03-28 19:37:32 +0900

    test

--- backjun/src/main/java/needClean/test2.java
+++ backjun/src/main/java/needClean/test2.java
@@ -15,6 +15,7 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class test2 {
+
 	//우좌상하 순서
 	static int[] dx = {0,0,-1,1};
 	static int[] dy = {1,-1,0,0};
@@ -42,8 +43,122 @@ public class test2 {
 			map = new int[n][m];
 			visit = new boolean[n][m];
 			for(int i = 0 ; i < n ; i++) {
-ppend("\n");
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0 ; j < m ; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int result = 0;
+			for(int i = 0 ; i < n ; i++) {
+				for(int j = 0 ; j < m ; j++) {
+					if( map[i][j] == 1 && !visit[i][j] ) {
+						result++;
+						visit[i][j] = true;
+						q = new ArrayDeque<>();
+						q.add(new Pos(i,j));
+						bfs();
+					}
+				}
+			}
+			sb.append(result).append("\n");
+			
+			
+=======
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		N = Integer.parseInt(st.nextToken()); // 체스판 크기
+		K = Integer.parseInt(st.nextToken()); // 말 개수
+
+		//체스판 입력 받기		
+		map = new int[N][N];
+		stack = new Stack[N][N];
+		for(int i = 0 ; i < N ; i++){
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0 ; j < N ; j++){
+				map[i][j] = Integer.parseInt(st.nextToken());
+				stack[i][j] = new Stack<Integer>();
+			}
+>>>>>>> branch 'master' of https://github.com/tak19/java-study2.git
+		}
+		
+<<<<<<< HEAD
+		System.out.println(sb);
+=======
+		
+		
+		//말의 정보를 입력 받는다! -> 행,열,이동방향 인덱스를 맞춰줌
+		horse = new Pos[K];
+		for(int i = 0 ; i < K ; i ++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			int dir = Integer.parseInt(st.nextToken()) - 1;
+			
+			horse[i] = new Pos(x,y,dir);
+			//스택에 말을 저장함
+			stack[x][y].add(i);
+		}
+		//구현 메소드
+		letSimul();
+		System.out.println(cnt >= 1000 ? -1 : cnt);
+>>>>>>> branch 'master' of https://github.com/tak19/java-study2.git
+	}
+<<<<<<< HEAD
+
+	private static void bfs() {
+		
+		while( !q.isEmpty() ) {
+			Pos tem= q.poll();
+			for(int i = 0 ; i < 8 ;i++) {
+				int gox = tem.x + dx[i];
+				int goy = tem.y + dy[i];
+				if( canGo(gox,goy) ) {
+					if( map[gox][goy] == 1 && !visit[gox][goy] ) {
+						visit[gox][goy] = true;
+						q.add(new Pos(gox,goy));
+					}
+				}
+			}
+		}
+		
+		
+	}
+	private static boolean canGo(int gox, int goy) {
+		if( gox >= 0 && gox < n && goy >= 0 && goy < m ) {
+			return true;
+		}
+		return false;
+	}
+	static class Pos{
+		int x, y;
+
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+=======
+	
+	//게임시작
+	private static void letSimul() {
+		cnt = 0;
+		while( cnt < 1000 ) {
+			cnt++;
+			//말 전체 이동이 한턴임
+			for(int i = 0 ; i < K ; i++) {
+				moveHorse(i);
+				if( ck ) {
+					break;
+				}
+			}
+			if( ck ) {
+				break;
+			}
+			
+		}
+		
+	}
 	//말을 움직인다.
 	private static void moveHorse(int num) {
 		int x = horse[num].x;
