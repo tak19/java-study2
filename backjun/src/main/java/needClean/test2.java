fca167d171e2be03a86b18f6cf4390ad3a09f361
tso5245 <tso5245@naver.com>
2023-03-29 17:58:48 +0900

    수정필요

--- backjun/src/main/java/needClean/test2.java
+++ backjun/src/main/java/needClean/test2.java
@@ -1,186 +1,204 @@
 package needClean;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+/*
+ * 대각선방향으로 진행된다...
+ * 두 x,y의 합으로 인덱스를 지정해서 List 배열에 저장한다.
+ */
 public class test2 {
-	//우좌상하 순서
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {1,-1,0,0};
-	static int N,K,cnt;
-	static int[][] map;
-	static boolean ck;
-	static List<Integer> list; //겹쳐진 숫자를 판단할 리스트임
-	static Pos[] horse;
-	static Stack[][] stack;
-	
-	public static void main(String[] args) throws Exception{
+	static int N,result,total;
+	static int map[][];
+	static boolean[][] possible;
+	static List<int[]>[] listBishop;
+	static int[] isSelect;
+	static Stack<int[]> queue,temp;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken()); // 체스판 크기
-		K = Integer.parseInt(st.nextToken()); // 말 개수
-
-		//체스판 입력 받기		
+		N = Integer.parseInt(br.readLine()); //체스판 크기
+		
+		//사선방향을 저장할 리스트 뽑음
+		int range = (2*N)-1;
+		listBishop = new ArrayList[range];
+		for(int i = 0 ; i < range ; i++ ) {
+			listBishop[i] = new ArrayList<>();
+		}
+		
+		//체스판을 입력받아유~
+		StringTokenizer st = null;
 		map = new int[N][N];
-		stack = new Stack[N][N];
-		for(int i = 0 ; i < N ; i++){
+		possible = new boolean[N][N];
+		for(int i = 0 ; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < N ; j++){
+			for(int j = 0 ; j < N ; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				stack[i][j] = new Stack<Integer>();
+				if( map[i][j] == 1 ) {
+					listBishop[i+j].add(new int[] {i,j});
+				}
 			}
 		}
+		//뽑았는지 확인하기 위한 배열
+		isSelect = new int[range];
+		//행-뽑은 수-남은 수 전달
+		positionBishop(0, 0, 0);
 		
-		
-		
-		//말의 정보를 입력 받는다! -> 행,열,이동방향 인덱스를 맞춰줌
-		horse = new Pos[K];
-		for(int i = 0 ; i < K ; i ++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
-			int dir = Integer.parseInt(st.nextToken()) - 1;
-			
-			horse[i] = new Pos(x,y,dir);
-			//스택에 말을 저장함
-			stack[x][y].add(i);
-		}
-		//구현 메소드
-		letSimul();
-		System.out.println(cnt >= 1000 ? -1 : cnt);
+
+		//		queue = new ArrayDeque<>();
+		//		temp = new ArrayDeque<>();
+//		queue = new Stack();
+//		temp = new Stack();
+//		// 완탐해봄 - 비숍을 넣을 수 있는 칸을 시작점으로 하나씩 완탐
+//		for(int i = 0 ; i < N; i++) {
+//			for(int j = 0 ; j < N ; j++) {
+//				if( map[i][j] == 1 ) {
+//					positionBishop(i,j,0,total--);
+//					queue.clear();
+//					temp.clear();
+//				}
+//			}
+//		}
+		System.out.println(result);
+
 	}
 	
-	//게임시작
-	private static void letSimul() {
-		cnt = 0;
-		while( cnt < 1000 ) {
+	private static void positionBishop(int row, int cnt, int remain) {
 
-			cnt++;
-			//말 전체 이동이 한턴임
-			for(int i = 0 ; i < K ; i++) {
-				moveHorse(i);
-				if( ck ) {
-					break;
-				}
-			}
-			if( ck ) {
-				break;
-			}
-			
+		if( result > cnt + remain ) {
+			return;
 		}
-		
-	}
-	//말을 움직인다.
-	private static void moveHorse(int num) {
-		int x = horse[num].x;
-		int y = horse[num].y;
-		//이동 시킬 말의 위치
-		int gox = horse[num].x + dx[horse[num].dir];
-		int goy = horse[num].y + dy[horse[num].dir];
-		//실제 방향을 확인함
-		horse[num].dir = selectDir(gox,goy,horse[num].dir);
-		// 실제 갈 방향을 더해서 좌표계산을 한다.
-		gox = horse[num].x + dx[horse[num].dir];
-		goy = horse[num].y + dy[horse[num].dir];
-		
-		//범위 안인지부터 체크함 + 파란색인지 확인
-		if( canGo(gox, goy) && map[gox][goy] != 2 ) {
-			//다음 이동 칸이 흰,빨 중에 하나인 경우만 계산
-			if( map[gox][goy] == 0 ) {
-				Queue<Integer> q = new ArrayDeque<>();
-				//흰색 이라면 -> 딸린 식구 데리고 -- 현재 스택에서 자기 번호가 나올때까지 다른 말을 이동시킴 
-				while( (int)stack[x][y].peek() != num ) {
-					q.add((int)stack[x][y].pop());
-				}
-				// 자신 값 옮긴 후에 나머지 값도 옮긴다.
-				stack[gox][goy].add(stack[x][y].pop());
-				horse[num].x = gox;
-				horse[num].y = goy;
-				
-				while( !q.isEmpty() ) {
-					int n = q.peek();
-					horse[n].x = gox;
-					horse[n].y = goy;
-					stack[gox][goy].add(q.poll());
-				}
-				
-				
-			}else {
-				//빨간색이라면
-				Queue<Integer> q = new ArrayDeque<>();
-				// 딸린 식구 데리고 -- 현재 스택에서 자기 번호가 나올때까지 다른 말을 이동시킴 
-				while( (int)stack[x][y].peek() != num ) {
-					q.add((int)stack[x][y].pop());
-				}
-				// 자신 값 옮긴 후에 나머지 값도 옮긴다.
-				stack[gox][goy].add(stack[x][y].pop());
-				horse[num].x = gox;
-				horse[num].y = goy;
-				
-				while( !q.isEmpty() ) {
-					int n = q.peek();
-					horse[n].x = gox;
-					horse[n].y = goy;
-					stack[gox][goy].add(q.poll());
-				}
-				//뒤집는 연산이 필요해
-				int size = stack[gox][goy].size();
-				Stack<Integer> tem = new Stack<>();
-				while( size --> 0 ) {
-					tem.add((int)stack[gox][goy].pop());
-				}
-				stack[gox][goy] = tem;
-			}
 
-			if(stack[gox][goy].size() >= 4 ) {
-				ck = true;
-				return;
-			}
-		}else { 
-			//범위 밖 그냥 제자리에 있음 -> 제자리에 있으면 스택과 변화가 없겠지
-			
+		//다 뽑았다면!!!
+		if( row >= N ) {
+			//print(cnt);
+			result = Math.max(result, cnt);
+			return;
 		}
-		
-		
-		
-	}
-	//이동 위치가 범위를 벗어나거나 파란색일 경우 방향을 바꿔준다.
-	private static int selectDir(int gox, int goy, int dir) {
-		if( !canGo(gox,goy) || map[gox][goy] == 2 ) {
-			if( dir <= 1 ) {
-				//우좌 방향이라면 반대전환
-				return dir == 1 ? 0 : 1;
-			}else {
-				//상하방항이라면 반대 전환
-				return dir == 2 ? 3 : 2;
+
+		//뽑은적 없는 사선이라면!!
+		for( int[] tem : listBishop[row] ) {
+			if( isSelect[tem[0] + tem[1]] == 0 ) {
+				isSelect[tem[0] + tem[1]] = 1;
+				positionBishop(row+2, cnt+1, remain+1);
+				isSelect[tem[0] + tem[1]] = 0;
 			}
 		}
-		return dir;
-	}
+		positionBishop(row+2, cnt, remain+1);
 
-	//범위 안인지
-	private static boolean canGo(int gox, int goy) {
-		if( gox >= 0 && gox < N && goy >= 0 && goy < N) {
-			return true;
+	}
+	
+	
+	
+//	// 놓을 수 있는 경우도 안 놓고 진행해야 하는 경우가 있을 수도 있음
+//	private static void positionBishop1(int row, int col, int cnt, int remain) {
+//
+//		if( result > cnt + remain ) {
+//			return;
+//		}
+//
+//		//다 뽑았다면!!!
+//		if( row >= N ) {
+//			//print(cnt);
+//			result = Math.max(result, cnt);
+//			return;
+//		}
+//		//해당 열 다 탐색 했다면 다음행 탐색함
+//		if( col >= N ) {
+//			positionBishop(row+1, 0, cnt,remain);
+//			return;
+//		}
+//		//비숍이 위치할 수 있는 칸이라면
+//		if( map[row][col] == 1  ) {
+//			if( !possible[row][col] ) {
+//				if( ckBishop(row, col) ) {
+//					moveBishop();
+//					//중복되는 칸이 없다면 해당 칸에 비숍을 넣던가 말던가 해야함
+//					int[] arr = new int[2];
+//					arr[0] = row;
+//					arr[1] = col;
+//					paint(arr);
+//					queue.add(arr);
+//					//다음열을 탐색하는데 비숍의 크기 하나 더해줌 뽑았으니까 cnt 증가
+//					positionBishop(row,col+1,cnt+1,remain-1);
+//					reversePaint(queue.pop());
+//					positionBishop(row,col+1,cnt,remain-1);
+//				}else {
+//					//중복되는 칸이 있다면 다음 열 탐색
+//					moveBishop();
+//					positionBishop(row,col+1,cnt,remain-1);
+//				}
+//			}else {
+//				positionBishop(row,col+1,cnt,remain-1);
+//			}
+//			
+//		}else {
+//			//비숍이 놓일 수 없는 칸이라면 다음 열 탐색
+//			positionBishop(row,col+1,cnt,remain);
+//		}
+//
+//
+//	}
+	private static void reversePaint(int[] arr) {
+		int cha = Math.abs(arr[0] - arr[1]);
+
+		for(int i = 0 ; i < N; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				if( cha == Math.abs(i-j) ) {
+					possible[i][j] = false;
+				}
+			}
 		}
-		return false;
 	}
-	//좌표 저장
-	static class Pos{
-		int x,y,dir;
+	//못가는칸임!
+	private static void paint(int[] arr) {
+		int cha = Math.abs(arr[0] - arr[1]);
 
-		public Pos(int x, int y, int dir) {
-			this.x = x;
-			this.y = y;
-			this.dir = dir;
+		for(int i = 0 ; i < N; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				if( cha == Math.abs(i-j) ) {
+					possible[i][j] = true;
+				}
+			}
+		}
+
+	}
+	//해당 위치 대각선에 비숍이 있는지 판단한다.
+	private static boolean ckBishop(int i, int j) {
+
+		//현재까지 뽑은 비숍이랑 현재 뽑을 비숍을 확인한다.
+		while( !queue.isEmpty() ) {
+			//int[] arr = queue.poll();
+			int[] arr = queue.pop();
+			temp.add(arr);
+			//현재 위치랑 뽑힌 비숍의 위치를 비교
+			if( Math.abs(arr[0] - i ) == Math.abs( arr[1] - j )  ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//큐에 있는 비숍 옮김
+	private static void moveBishop() {
+		while( !temp.isEmpty() ) {
+			//			queue.offer(temp.poll());
+			queue.add(temp.pop());
+		}
+	}
+
+	private static void print(int cnt) {
+		if( cnt == 7 ) {
+			while( !queue.isEmpty() ) {
+				//int[] a = queue.poll();
+				int[] a = queue.pop();
+				System.out.println(a[0] + " " + a[1] );
+			}
+			System.out.println("ddddddddddddd");
 		}
 	}
 
 }
+
+
