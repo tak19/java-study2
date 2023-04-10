39ab4d97765aba3b0ebd0a13bc1294c238fa5fd1
tso5245 <tso5245@naver.com>
2023-04-10 09:04:38 +0900

    수정필요

--- backjun/src/main/java/needClean/ti.java
+++ backjun/src/main/java/needClean/ti.java
@@ -6,92 +6,54 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/*
- * 테두리 한바퀴 싸이클 돌리고 찾은 열쇠가 있다면 다음 배열에 찾을 열쇠값이 있다고 표시함
- * 다음 연산때 다른 열쇠를 찾은 적이 없다면 종료하고 값이 있다면 다음탐색도 계속 진행함
- * 대문자에서 65를 빼주면 0인덱스부터 시작.. 소문자는 97을 빼주면 0인덱스부터 시작
- */
-
 public class ti {
-	static int N,startX;
-	static long ans,result;
-	static int[] input;
+	//상우하좌
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	static int N,M,K;
+	static boolean[][] visit;
 	static int[][] map;
-	static boolean[] visit;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-
-		//연결정보 입력받음
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // map size
+		M = Integer.parseInt(st.nextToken()); // player Su
+		K = Integer.parseInt(st.nextToken()); // round
+		
+		//지도 입력받기
 		map = new int[N][N];
-		StringTokenizer st = null;
 		for(int i = 0 ; i < N ; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0 ; j < N ; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		//출발점을 바꿔가며 진행한다.
-		ans = 0;
-		for(int i = 0 ; i < N ; i++) {
-			visit = new boolean[N];
-			visit[i] = true;
-			result = 0;
-			startX = i;
-			dp(i,0);
-			ans = Math.min(ans, result);
-		}
 		
-		System.out.println(ans);
+		
+		
 	}
-	//탐색을 시작한다. index는 자신 행의 인덱스를 나타냄
-	private static void dp(int index, int cnt) {
-		//System.out.println(result + " " + ans + " " + cnt);
-		if( ans > 0 && ans < result ) {
-			return;
+
+	//범위 안인지
+	private static boolean canGo(int gox, int goy) {
+		if( gox >= 0 && gox < N && goy >= 0 && goy < N ) {
+			return true;
 		}
-		//다 뽑았다면
-		if( cnt >= (N-1) ) {
-			//System.out.println("다뽑음");
-			//마지막 도시도 출발 도시랑 붙어 있어야한다.
-			if( map[index][startX] > 0 ) {
-				result += map[index][startX];
-				if( ans == 0 ) {
-					//System.out.println("ㅇㅇㅇ");
-					ans = result;
-				}else {
-					
-					//System.out.println("dfkmasfke");
-					ans = Math.min(ans, result);
-				}
-			}
-			return;
+		return false;
+	}
+	
+	//좌표정보 - 위치, 기본능력치, 방향, 권총 저장
+	static class Pos{
+		int x,y,basic,dir,him;
+
+		public Pos(int x, int y, int basic, int dir, int him) {
+			this.x = x;
+			this.y = y;
+			this.basic = basic;
+			this.dir = dir;
+			this.him = him;
 		}
 
-		int min = Integer.MAX_VALUE;
-		int nextRow = -1;
-		//다음 방문할 도시를 선택함 --> 가중치가 가장 낮은놈을 선택함
-		for(int i = 0; i < N ; i++) {
-			//방문하지 않았다면 + 자기 자신의 인덱스가 아니라면
-			if( !visit[i] && index != i ) {
-				//자신의 도시와 연결되어있는 도시중에 가장 작은 도시를 선택한다.
-				if( map[index][i] != 0 ) {
-					if( min > map[index][i] ) {
-						min = map[index][i];
-						nextRow = i;
-					}
-				}
-			}
-		}
 		
-		if( nextRow == -1 ) {
-			result += Integer.MAX_VALUE;
-			return;
-		}else {
-			visit[nextRow] = true;
-			result += min;
-			dp(nextRow,cnt+1);
-		}
-
+		
 	}
 }
