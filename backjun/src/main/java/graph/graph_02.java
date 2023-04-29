ec7a66b01d11d4b7e5816e6370d3259d9c9e8f2c
tso5245 <tso5245@naver.com>
2023-04-30 02:29:01 +0900

    Graph - 가장 가까운 공통조상(3584)

--- backjun/src/main/java/graph/graph_02.java
+++ backjun/src/main/java/graph/graph_02.java
@@ -4,102 +4,72 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-/*
- * 여행경로가 이어져있는지만 확인하면 될듯 아무 정점 시작해서
- */
-public class graph_02 {
-	static int N,M,now;
-	static Node[] node;
-	static int[] list;
-	static boolean[][] connect;
 
+public class graph_02 {
+	static int N;
+	static Node[] node;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st = null;
-
-		N = Integer.parseInt(br.readLine()); //관계수
-		M = Integer.parseInt(br.readLine()); //여행 포함 수
-
-		node = new Node[N+1];
-		connect = new boolean[N+1][N+1];
-		//관계를 입력
-		for(int i = 1 ; i <= N ; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 1 ; j <= N ; j++) {
-				int from = Integer.parseInt(st.nextToken());
-				//연결되어 있다면
-				if( from != 0 ) {
-					node[i] = new Node(j,node[i]);
-					node[j] = new Node(i,node[j]);
-				}
-				if( i == j ) {
-					connect[i][j] = true;
-				}
+		StringBuilder sb = new StringBuilder();
+		for(int test = 0 ; test < T ; test++) {
+			N = Integer.parseInt(br.readLine()); //노드수
+			//그래프 정보를 입력받는다
+			node = new Node[N+1];
+			for(int i = 1 ; i < N ; i++) {
+				st = new StringTokenizer(br.readLine());
+				int parent = Integer.parseInt(st.nextToken()); //부모
+				int child = Integer.parseInt(st.nextToken()); //자식
+				//부모쪽으로 올라가는 간선을 만든다.
+				node[child] = new Node(parent, child, node[child]);
 			}
-		}
-		
-		for(int i = 1 ; i <= N ; i++) {
-			now = i;
-			dfs(i);
-		}
-		if( M != 1) {
 			
+			//탐색할 노드 입력받기
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
+			
+			//방문 체크 배열
+			boolean[] visit = new boolean[N+1];
+			int to = 0;
+			visit[v1] = true;
+			for(Node tem = node[v1] ; tem != null ; tem = node[to]) {
+				visit[tem.to] = true;
+				to = tem.to;
+			}
+			//v1 경유 중 v2를 방문한 이력이 있다면 바로 종료!
+			if( visit[v2] ) {
+				sb.append(v2).append("\n");
+				continue;
+			}
+			//아니라면 v2 를 탐색하면서 방문 이력이 있는 노드를 찾음
+			to = 0;
+			for(Node tem = node[v2] ; tem != null; tem = node[to]) {
+				to = tem.to;
+				if(visit[to]) {
+					sb.append(to).append("\n");
+					break;
+				}
+			}
 		}
-		st = new StringTokenizer(br.readLine());
-		list = new int [M+1];
-		for(int i = 1 ; i <= M; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-		}
+		System.out.println(sb);
 		
-		//print();
-		
-		String result = "YES";
-		for(int i = 2 ; i <= M; i++) {
-			if( !connect[list[1]][list[i]] ) {
-				result = "NO";
-			}
-		}
-		
-		System.out.println(result);
-	}
-	
-	private static void print() {
-		for(int i = 1 ; i <= N ; i++) {
-			for(int j = 1 ; j <= N ; j++) {
-				System.out.print(connect[i][j] + "\t");				
-			}
-			System.out.println();
-		}
-	}
-	
-	private static void dfs(int current) {
-		for( Node temp = node[current]; temp != null ; temp = temp.node ) {
-			if ( !connect[now][temp.from] ) {
-				connect[now][temp.from] = true;
-				dfs(temp.from);
-			}
-		}
-	}
-	//순서 확인함
-	private static boolean ck(int from) {
-		//뒷순서가 먼저 나오면 안된다!!
-		for(int i = (now+1) ; i <= M ; i++) {
-			if( list[i] == from ) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	// 관계를 저장
+	}
+	
+	//정점을 나타냄
 	static class Node{
-		int from;
+		int to,me;
 		Node node;
-
-		public Node(int from, Node node) {
-			this.from = from;
+		
+		public Node(int to, int me, Node node) {
+			this.to = to;
+			this.me = me;
 			this.node = node;
 		}
 
+		
 	}
 }
