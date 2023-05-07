6ea7c7a57068882e37b067cc85c68bf459fe2267
tso5245 <tso5245@naver.com>
2023-05-07 23:29:17 +0900

    bruteforce - 감소하는 수(1038)

--- backjun/src/main/java/bruteforce/bruteforce_15.java
+++ backjun/src/main/java/bruteforce/bruteforce_15.java
@@ -2,6 +2,7 @@ package bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -11,49 +12,30 @@ public class bruteforce_15 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long result = 0;
 		
-		//10보다 작거나 같으면 해당 값이 결과 값임
-		if( N <= 10 ) {
-			System.out.println(N);
+		long[] arr = new long[1023];
+		// 0-9 까지의 부분집합으로 계산함.
+
+		for(int i = 1 ; i <= 1023 ; i++) {
+			long num = 0;
+			int tem_i = i;
+			//차례대로 9부터 넣음 -- 감소하는 수를 만들기 위함
+			for(int n = 9 ; n >= 0 ; n--) {
+				if( tem_i % 2 == 1 ) {
+					num = num * 10 + n; //해당 자리수를 뽑아 10진법으로 바로 전환
+				}
+				tem_i /= 2;
+			}			
+			arr[i-1] = num;
+		}
+		Arrays.sort(arr);
+		
+		
+		if( N >= 1023 ) {
+			System.out.println(-1);
 		}else {
-			//감소하는 수 찾기
-			int num = 10;
-			long start = 11;
-			while( num <= 1_000_000 ) {
-
-				//감소하는 수인지 판단함
-				if( ck(start) ) {
-					num++; //감소하는 수 증가 시켜줌
-				}
-				//해당 번째의 감소하는 수를 찾는다면
-				if( num == N ) {
-					result = start;
-					break;
-				}
-				 
-				
-				start++; //수 증가
-			}
-			result = result == 0 ? -1 : result;
-			System.out.println(result );
+			System.out.println(arr[N]);
 		}
 		
-		
-	}
-	//감소하는 수 인지 판단함
-	private static boolean ck(long start) {
-		String s = start+"";
-		for(int i = s.length()-1 ; i >= 1 ; i--) {
-			int be = s.charAt(i) - '0';
-			int fi = s.charAt(i-1) - '0';
-
-			//뒤에 수가 더 크다면
-			if( be >= fi ) {
-				return false;
-			}
-			
-		}
-		return true;
 	}
 }
