837fe7ba30998f8657bb4cac6eff8622070fb77a
tso5245 <tso5245@naver.com>
2023-05-25 23:37:57 +0900

    새싹 출력

--- backjun/src/main/java/backjun/practice_01.java
+++ backjun/src/main/java/backjun/practice_01.java
@@ -8,16 +8,8 @@ import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-/*
-문자열의 뒤에 A를 추가한다.
-문자열을 뒤집고 뒤에 B를 추가한다.
- */
 public class practice_01 {
-	static int tem;
-	static boolean[] select;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringBuilder sb = new StringBuilder();
 		sb.append("         ,r'\"7").append("\n");
 		sb.append("r`-_   ,'  ,/").append("\n");
