3ad3ddf92c3de157e1fe0ade3f2b9809fa35c503
tso5245 <tso5245@naver.com>
2023-06-01 20:05:16 +0900

    새싹 출력

--- backjun/src/main/java/backjun/practice_01.java
+++ backjun/src/main/java/backjun/practice_01.java
@@ -1,12 +1,4 @@
 package backjun;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class practice_01 {
 	public static void main(String[] args) throws Exception {
