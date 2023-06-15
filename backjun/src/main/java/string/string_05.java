ad3f3d145efc4489cc8f7ebc2a47041fdc626ab8
tso5245 <tso5245@naver.com>
2023-06-16 00:17:17 +0900

    String - DNA(1969)

--- backjun/src/main/java/string/string_05.java
+++ backjun/src/main/java/string/string_05.java
@@ -8,19 +8,66 @@ import java.util.TreeMap;
 
 public class string_05 {
 	static int[] arr;
-	static int n;
+	static String[] dnaArr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		//DNA 입력받기
-		String[] dna = new String[n]; 
+		dnaArr = new String[n]; 
 		for(int i = 0 ; i < n ; i++) {
-			dna[i] = br.readLine();
+			dnaArr[i] = br.readLine();
 		}
+		int Distance = 0;
+		StringBuilder sb = new StringBuilder();
+		//한자리씩 빈도 수 알아보기 - A,C,T,G 순서대로 이루어짐
+		for(int i = 0 ; i < m ; i++) {
+			arr = new int[4];
+			//모든 DNA 단어 방문
+			for(int j = 0 ; j < n ; j++) {
+				countDNA(i,j);
+			}
+			//최대값 저장함
+			int maxCnt = 0;
+			int select = 0;
+			for( int z = 0 ; z < 4 ; z++ ) {
+				if( maxCnt < arr[z] ) {
+					maxCnt = arr[z];
+					select = z;
+				}
+			}
+			Distance += (n - maxCnt);
+			appendChar(select,sb);
+			
+		}
+		sb.append("\n").append(Distance);
+		System.out.println(sb);
 		
-		
-		
+	}
+	private static void appendChar(int select, StringBuilder sb) {
+		//문자 추가하기
+		if( select == 0 ) {
+			sb.append('A');
+		}else if ( select == 1 ) {
+			sb.append('C');
+		}else if ( select == 2 ) {
+			sb.append('G');
+		}else {
+			sb.append('T');
+		}
+	}
+	// DNA 계산하기
+	private static void countDNA(int index, int row) {
+		char dna = dnaArr[row].charAt(index);
+		if( dna == 'A' ) {
+			arr[0]++;
+		}else if(dna == 'C' ) {
+			arr[1]++;
+		}else if(dna == 'G' ) {
+			arr[2]++;
+		}else {
+			arr[3]++;
+		}
 	}
 }
