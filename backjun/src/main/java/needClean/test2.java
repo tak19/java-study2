1b77615bae480eb03448480479a4abaf902851ef
tso5245 <tso5245@naver.com>
2023-07-08 21:22:57 +0900

    Practice Code

--- backjun/src/main/java/needClean/test2.java
+++ backjun/src/main/java/needClean/test2.java
@@ -1,23 +1,29 @@
 package needClean;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-
+import java.util.StringTokenizer;
 
 public class test2 {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[31];
-		for(int i = 0 ; i < 28 ; i++) {
-			int tem = Integer.parseInt(br.readLine());
-			arr[tem]++;
+		int all = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = null;
+		int cal = 0;
+		for(int i = 0 ; i < n ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int price = Integer.parseInt(st.nextToken());
+			int cnt = Integer.parseInt(st.nextToken());
+			
+			cal += (price * cnt);
+			
 		}
-		for(int i = 1 ; i <= 30 ; i++) {
-			if( arr[i] == 0 ) {
-				System.out.println(i);
-			}
+		if( all == cal ) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
 		}
+		
 	}
 }
