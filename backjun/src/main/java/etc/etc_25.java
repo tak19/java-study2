7eceb33c78556d160dfd8dcf49862397df3ab253
tso5245 <tso5245@naver.com>
2023-07-09 18:28:03 +0900

    슬라이딩 윈도우 - 회전 초밥(15961)

--- backjun/src/main/java/etc/etc_25.java
+++ backjun/src/main/java/etc/etc_25.java
@@ -1,6 +1,7 @@
 package etc;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class etc_25 {
@@ -18,51 +19,44 @@ public class etc_25 {
 		
 		arr = new int[N];
 		window = new int[D+1]; //초밥 가지수 만큼의 윈도우
-		//초밥 입력 받았음
+		//초밥 종류를 입력 받았음 - 인덱스는 순서
 		for(int i = 0 ; i < N ; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		int select = 0;
-		//초기 슬라이딩 윈도우만큼 스시를 추가함
-		for(int i = 0 ; i < K ; i++) {
-			if( window[arr[i]] == 0 ) {
-				select++; //초밥 종류 증가 시킴
-			}
-			window[arr[i]]++;
-		}
-		//초기 시작점의 초밥 개수 세팅
-		int max = select;
-		
-		
-		//나머지 경우를 탐색해야하는경우 - 슬라이딩 윈도우 사용
-		for(int i = 1 ; i < N ; i++) {
-			if( max <= select ) {
-				//쿠폰 안 초밥 안 먹었다면 추가
-				if( window[C] == 0 ) {
-					max = select + 1;
-				}else {
-					max = select;
-				}
-				
-			}
-			//끝 인덱스 원소 더하고 앞쪽 인덱스 원소 빼줌
-			int addIndex = (i + K-1) % N;
-			if( window[arr[addIndex]] == 0 ) {
-				select++;
-			}
-			window[arr[addIndex]]++;
-			
-			window[arr[i-1]]--;
-			if(window[arr[i-1]]==0) {
-				select--;
-			}
-				
-			
-		}
-		
-		System.out.println(max);
-	}
+		int max = 0;
+        HashSet<Integer> sushiSet = new HashSet<>();
+        // 초기 슬라이딩 윈도우 설정 - 종류 별로 카운팅 증가 -> 중복은 증가 안함
+        for (int i = 0; i < K; i++) {
+            window[arr[i]]++;
+            if (window[arr[i]] == 1) {
+                select++;
+            }
+            sushiSet.add(arr[i]);
+        }
+        //최대값을 현재 종류와 쿠폰을 확인한 종류로 갱신
+        max = select + (sushiSet.contains(C) ? 0 : 1);
 
+        // 슬라이딩 윈도우 이동하며 최대 초밥 종류 개수 갱신
+        for (int i = K; i < N + K; i++) {
+            int removeIdx = (i - K) % N; // 제거 초밥 인덱스
+            window[arr[removeIdx]]--;
+            if (window[arr[removeIdx]] == 0) {
+                select--;
+                sushiSet.remove(arr[removeIdx]);
+            }
+            //추가 초밥 인덱스
+            int addIdx = i % N;
+            window[arr[addIdx]]++;
+            if (window[arr[addIdx]] == 1) {
+                select++;
+                sushiSet.add(arr[addIdx]);
+            }
 
+            max = Math.max(max, select + (sushiSet.contains(C) ? 0 : 1));
+        }
+
+        System.out.println(max);
+    }
 }
