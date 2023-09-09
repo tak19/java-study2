ec80aa3301f55b2e1942f2705ac08bb9e60331cd
tso5245 <tso5245@naver.com>
2023-09-09 12:53:33 +0900

    BFS_DFS

--- backjun/src/main/java/backjun/BFS_DFS.java
+++ backjun/src/main/java/backjun/BFS_DFS.java
@@ -38,8 +38,7 @@ public class BFS_DFS {
 		}
 		
 		StringBuilder sb = new StringBuilder();
-
-		
+	
 		//list 크기 만큼 반복... 만약 리스안에 값이 없다면 다음 리스트로 돌아감..
 		for(int i = 0 ; i < list.length ; i ++) {
 			//리스트에 값이 없다면 다음 노드의 간선을 확인함
