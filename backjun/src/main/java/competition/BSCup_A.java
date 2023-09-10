5e816d549fb7f45f903b18d33fe14fed2064af88
tso5245 <tso5245@naver.com>
2023-09-10 22:21:45 +0900

    브실컵 - A,브실이와 친구가 되고 싶어🤸‍♀️

---
+++ backjun/src/main/java/competition/BSCup_A.java
@@ -0,0 +1,35 @@
+package competition;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BSCup_A {
+	static StringBuilder sb = new StringBuilder();
+	//static int N,M;
+	//static int[] arr;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		//범위 구함
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		//k와 범위
+		st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		
+		int result = 0;
+		for(int i = a ; i <= b ; i++) {
+			if( Math.abs(i-k) <= x ) {
+				result++;
+			}
+		}
+		
+		if( result > 0 ) {
+			System.out.println(result);
+		}else {
+			System.out.println("IMPOSSIBLE");
+		}
+	}
+}
