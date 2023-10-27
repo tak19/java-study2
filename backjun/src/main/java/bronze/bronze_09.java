cbadbcd92333bbcc78f4195ee11a507720c95975
tso5245 <tso5245@naver.com>
2023-10-27 16:42:25 +0900

    B4 - 헛간 청약(19698)

---
+++ backjun/src/main/java/bronze/bronze_09.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bronze_09 {
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int cow = (W / L) * (H / L);
+		
+		if(cow >= N) {
+			System.out.println(N);
+		}else {
+			System.out.println(cow);
+		}
+		
+	}
+
+}
