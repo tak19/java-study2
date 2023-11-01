fd1d714a1212974b35efc70546ae10383b717912
tso5245 <tso5245@naver.com>
2023-11-01 23:49:40 +0900

    B4 - 자동완성(24883)

---
+++ backjun/src/main/java/bronze/bronze_13.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class bronze_13 {
+    public static void main(String[] args) throws Exception {
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         System.out.println(br.readLine().toLowerCase().charAt(0) == 'n' ? "Naver D2" : "Naver Whale");
+     }
+ }
