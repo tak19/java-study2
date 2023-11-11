32f98092dcf660b4fc0ba7e245550413183c9d02
tso5245 <tso5245@naver.com>
2023-11-11 22:05:46 +0900

    B4 - 빵(25377)

---
+++ backjun/src/main/java/bronze/bronze_17.java
@@ -0,0 +1,22 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bronze_17 {
+	private static final int INF = Integer.MAX_VALUE;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int min = INF;
+		while (n-->0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a>b) continue;
+			min = Math.min(min, b);
+		}
+		System.out.println(min == INF ? -1 : min);
+	}
+}
